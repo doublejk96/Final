00e9b75e349495bb827c268e8cde69b20d64da50
doublejk96 <66077060+doublejk96@users.noreply.github.com>
2020-10-22 17:26:03 +0900

    상자 파괴

---
+++ Assets/Scripts/Map/Box.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Box : MonoBehaviour
+{
+    public GameObject destoryEffect;
+
+    public List<GameObject> itemPrefab;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Bullet")
+        {
+            Instantiate(destoryEffect, transform.position, Quaternion.identity);
+
+            int i = Random.Range(0, itemPrefab.Count);
+            Instantiate(itemPrefab[i], transform.position, transform.rotation);
+
+            Destroy(gameObject);
+        }
+    }
+}
