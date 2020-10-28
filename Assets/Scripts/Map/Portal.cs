971b02d57548d924a47d461ef62d8aab8b96f2fa
doublejk96 <66077060+doublejk96@users.noreply.github.com>
2020-10-28 17:47:55 +0900

    로비 구현

---
+++ Assets/Scripts/Map/Portal.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Portal : MonoBehaviour
+{
+    public bool isEntrance = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            if (isEntrance == false)
+            {
+                SceneManager.LoadScene(2);
+                isEntrance = true;
+            }
+
+            if (isEntrance == true)
+            {
+                // 방이동
+            }
+        }
+    }
+}
